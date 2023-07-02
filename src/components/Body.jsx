e4feaa8a23c30295c66ef6ae221862291cbe726d
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-07-02 15:09:28 -0400

    first COMMIT

---
+++ src/components/Body.jsx
@@ -0,0 +1,25 @@
+export default function Header() {
+    return (
+        <header>
+            <h2>"There's always tomorrow for dreams to come true." - Rudolph the Red-Nosed Reindeer  
+            </h2>
+
+    
+    <main>
+        <div className="container">
+
+          <table className="text">
+            Rudolph1
+            </table>
+              <table>
+            Rudolph2
+              </table>
+            <table>
+            Rudolph3
+          </table>
+
+        </div>
+    </main>
+        </header>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Rudolph's Radiant Journey: The Red-Nosed Reindeer</h1>
+        </header>
+    )
+}
