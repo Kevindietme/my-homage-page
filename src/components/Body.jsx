3a5aad971d49aa7ae2b82ac9a830ebb8b2dc4105
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-07-02 19:20:06 -0400

    Cleaned up css and flex direction

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,30 +1,21 @@
-export default function Body() {
+const body = () => {
+    
     return (
+        
+        <div className="bodyHeader">
+                <main>
         <header>
             <h2>"There's always tomorrow for dreams to come true." - Rudolph the Red-Nosed Reindeer  
             </h2>
-        <p>
-            <img src="../src/images/rudolph.jpg"></img>
-        </p>
-    
-        <div className="container">
-    
-
-        <section className="card">
-            <section>
-            <ul>Rudolph1</ul>
-            
-              
-            <ul>Rudolph2</ul>
-             
-            
-            <ul>Rudolph3</ul>
-          </section>
-
-        </section>
-
-        </div>
-    
         </header>
-    )
-}
+            <div className="cards"/>
+                <h2>Rudolph1</h2>
+                <h2>Rudolph2</h2>
+                <h2>Rudolph3</h2>
+    </main>
+        </div>
+
+        )
+    };
+    
+    export default body 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 export default function Footer() {
 
+const link = "https://github.com/Kevindietme/my-homage-page";
+return (
+<a href = { link }>GitHub</a>
+
+)
 
     }
 
